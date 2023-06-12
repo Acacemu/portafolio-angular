9c34abd9d9341d92e804592fc189a98b36eb81fe
Adrian Ceballos <acacemu@gmail.com>
2023-06-12 11:38:12 -0500

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_Corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
